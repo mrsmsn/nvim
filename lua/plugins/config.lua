49945385729df9bc86c4fe61f90220f3a430e7e3
mrsmsn <53550779+mrsmsn@users.noreply.github.com>
2025-11-16 01:37:48 +0900

    Mod picker for hidden files

---
+++ lua/plugins/config.lua
@@ -0,0 +1,13 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    picker = {
+      sources = {
+        grep = {
+          hidden = true,
+          ignored = false,
+        },
+      },
+    },
+  },
+}
